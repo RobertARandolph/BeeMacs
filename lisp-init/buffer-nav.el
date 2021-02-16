2424886155e0cc591427edaf13522f3c1c0f1494
Robert Randolph <audiolabs@gmail.com>
2021-02-15 20:29:02 -0500

    change avy to keyboard friendly

--- lisp-init/buffer-nav.el
+++ lisp-init/buffer-nav.el
@@ -6,6 +6,19 @@
 (use-package avy
   :after evil
   :config (progn
-            (define-key evil-motion-state-map (kbd "C-;") #'avy-goto-char-2)
-            (setq avy-keys (nconc (number-sequence ?1 ?9)
-                                  '(?0)))))
+            (define-key evil-motion-state-map (kbd "s-f") #'avy-goto-char-2)
+            (setq avy-keys '(?n ?t ?e ?s ?i ?r ?o ?a))))
+
+(defun avy-goto-parens ()
+  (interactive)
+  (let ((avy-command this-command))   ; for look up in avy-orders-alist
+    (beginning-of-defun)
+    (let ((beg (point)))
+      (end-of-defun)
+      (let ((end (point)))
+        (avy-jump "(+\\|\\[+\\|{+" :window-flip nil
+                  :beg beg
+                  :end end)))))
+(add-to-list 'avy-orders-alist '(avy-goto-parens . avy-order-closest))
+
+(global-set-key (kbd "s-p") 'avy-goto-parens)
