5abdc35e58cd144cdc3722b9ed4d095af78dc850
Robert Randolph <audiolabs@gmail.com>
2021-10-29 15:53:19 -0400

    switch to aquamacs-like mode

--- lisp-init/lisp.el
+++ lisp-init/lisp.el
@@ -3,40 +3,35 @@
 (use-package flycheck-clj-kondo
   :ensure t)
 
-(use-package clojure-mode
-  :ensure t
-  :config
-  (require 'flycheck-clj-kondo))
-
 ;; (add-to-list 'load-path "/Users/robertrandolph/Documents/Clojure/inf-clojure")
-(use-package inf-clojure
-  :config (progn
-            (define-key clojure-mode-map (kbd "C-M-x") 'inf-clj-eval-defun) ;; primary eval command
-            (define-key clojure-mode-map (kbd "C-c C-e") 'inf-clj-eval-defun)
-            (define-key clojure-mode-map (kbd "C-x C-e") 'inf-clojure-eval-last-sexp)
-            (define-key clojure-mode-map (kbd "C-c C-l") 'inf-clojure-eval-buffer)
-            (define-key clojure-mode-map (kbd "C-c C-r") 'inf-clojure-eval-region)
-            (define-key clojure-mode-map (kbd "C-c C-v") 'inf-clojure-show-var-documentation)
-            (define-key clojure-mode-map (kbd "C-c C-s") 'inf-clojure-show-var-source)
-            (setq inf-clojure-repl-features
-                 '((clojure . ((load . "(clojure.core/load-file \"%s\")")
-                               (doc . "(clojure.repl/doc %s)")
-                               (source . "(clojure.repl/source %s)")
-                               (arglists .
-                           "(try
-                             (:arglists
-                              (clojure.core/meta
-                               (clojure.core/resolve
-                                (clojure.core/read-string \"%s\"))))
-                            (catch #?(:clj Throwable :cljr Exception) e nil))")
-                               (apropos . "(doseq [var (sort (clojure.repl/apropos \"%s\"))] (println (str var)))")
-                               (ns-vars . "(clojure.repl/dir %s)")
-                               (set-ns . "(clojure.core/in-ns '%s)")
-                               (macroexpand . "(clojure.core/macroexpand '%s)")
-                               (macroexpand-1 . "(clojure.core/macroexpand-1 '%s)")))))
-                 (setq inf-clojure-startup-forms '((clojure . "clojure")
-                                                   (clojure . "clojure -A:dev")))
-                 (advice-add #'inf-clojure :after '(lambda (_) (setq comint-scroll-to-bottom-on-output t)))))
+;;(use-package inf-clojure
+;;  :config (progn
+;;            (define-key clojure-mode-map (kbd "C-M-x") 'inf-clj-eval-defun) ;; primary eval command
+;;            (define-key clojure-mode-map (kbd "C-c C-e") 'inf-clj-eval-defun)
+;;            (define-key clojure-mode-map (kbd "C-x C-e") 'inf-clojure-eval-last-sexp)
+;;            (define-key clojure-mode-map (kbd "C-c C-l") 'inf-clojure-eval-buffer)
+;;            (define-key clojure-mode-map (kbd "C-c C-r") 'inf-clojure-eval-region)
+;;            (define-key clojure-mode-map (kbd "C-c C-v") 'inf-clojure-show-var-documentation)
+;;            (define-key clojure-mode-map (kbd "C-c C-s") 'inf-clojure-show-var-source)
+;;            (setq inf-clojure-repl-features
+;;                 '((clojure . ((load . "(clojure.core/load-file \"%s\")")
+;;                               (doc . "(clojure.repl/doc %s)")
+;;                               (source . "(clojure.repl/source %s)")
+;;                               (arglists .
+;;                           "(try
+;;                             (:arglists
+;;                              (clojure.core/meta
+;;                               (clojure.core/resolve
+;;                                (clojure.core/read-string \"%s\"))))
+;;                            (catch #?(:clj Throwable :cljr Exception) e nil))")
+;;                               (apropos . "(doseq [var (sort (clojure.repl/apropos \"%s\"))] (println (str var)))")
+;;                               (ns-vars . "(clojure.repl/dir %s)")
+;;                               (set-ns . "(clojure.core/in-ns '%s)")
+;;                               (macroexpand . "(clojure.core/macroexpand '%s)")
+;;                               (macroexpand-1 . "(clojure.core/macroexpand-1 '%s)")))))
+;;                 (setq inf-clojure-startup-forms '((clojure . "clojure")
+;;                                                   (clojure . "clojure -A:dev")))
+;;                 (advice-add #'inf-clojure :after '(lambda (_) (setq comint-scroll-to-bottom-on-output t)))))
 
 ;; Slow, I don't yet understand value over plain clj-kondo
 ;;(use-package eglot
@@ -81,16 +76,17 @@
 
 (use-package clojure-mode
   :config (progn
-            ;;(require 'flycheck-clj-kondo)
-            ;;(define-key clojure-mode-map (kbd "C-z") 'run-clojure-no-prompt)
-            ;;(define-key clojure-mode-map (kbd "C-c C-z") 'run-clojure)
-            ;;(define-key clojure-mode-map (kbd "C-M-x") 'lisp-eval-defun) ;; primary eval command
-            ;;(define-key clojure-mode-map (kbd "C-c C-e") 'lisp-eval-defun)
-            ;;(define-key clojure-mode-map (kbd "C-x C-e") 'lisp-eval-last-sexp)
-            ;;(define-key clojure-mode-map (kbd "C-c C-l") 'lisp-load-buffer)
-            ;;(define-key clojure-mode-map (kbd "C-c C-n") 'lisp-eval-form-and-next)
-            ;;(define-key clojure-mode-map (kbd "C-c C-p") 'lisp-eval-paragraph)
-            ;;(define-key clojure-mode-map (kbd "C-c C-r") 'lisp-eval-region)
+            (require 'flycheck-clj-kondo)
+            (define-key clojure-mode-map (kbd "C-z") 'run-clojure-no-prompt)
+            (define-key clojure-mode-map (kbd "C-c C-z") 'run-clojure)
+            (define-key clojure-mode-map (kbd "C-M-x") 'clj-eval-defun) ;; primary eval command
+            (define-key clojure-mode-map (kbd "C-M-S-x") 'clj-eval-defun-and-print)
+            (define-key clojure-mode-map (kbd "C-c C-e") 'lisp-eval-defun)
+            (define-key clojure-mode-map (kbd "C-x C-e") 'lisp-eval-last-sexp)
+            (define-key clojure-mode-map (kbd "C-c C-l") 'lisp-load-buffer)
+            (define-key clojure-mode-map (kbd "C-c C-n") 'lisp-eval-form-and-next)
+            (define-key clojure-mode-map (kbd "C-c C-p") 'lisp-eval-paragraph)
+            (define-key clojure-mode-map (kbd "C-c C-r") 'lisp-eval-region)
             (setq clojure-refactor-map-prefix (kbd "s-l"))
             (defvar clojure-refactor-map
               (let ((map (make-sparse-keymap)))
@@ -120,21 +116,45 @@
                 map)
               "Keymap for Clojure refactoring commands.")
             (define-key clojure-mode-map clojure-refactor-map-prefix 'clojure-refactor-map)
-            (define-key clojure-mode-map (kbd "C-c C-z") 'inf-clojure)
+            ;;(define-key clojure-mode-map (kbd "C-c C-z") 'inf-clojure)
             (add-hook 'clojure-mode-hook 'lisp-mode-hooks)))
 
 (setq clojure-align-forms-automatically t)
 
-(comment
  (setq inferior-lisp-program "clojure")
 
  (defvar clj-repl-command)
+ (defvar clj-repl-host)
+ (defvar clj-repl-port)
+
+;; Buffer eval'd from
+ (defvar clojure-current-buffer)
+;; Point of end of function eval'd
+ (defvar clojure-last-eval-function-end)
 
  (defvar clj-repl-command-history '())
 
  (savehist-mode 1)
 
  (add-to-list 'savehist-additional-variables 'clj-repl-command-history)
+
+;; Do things after 
+(add-hook 'comint-output-filter-functions #'clojure-preoutput-filter)
+
+(defun clojure-preoutput-filter (str)
+  "Preprocess the output STR from interactive commands."
+  (if clojure-current-buffer ;; eval command sets the current buffer if it's an "print to buffer" type command
+      (with-current-buffer clojure-current-buffer
+        (save-excursion
+          (goto-char clojure-last-eval-function-end)
+          (let* ((output (replace-regexp-in-string "^" ";; " ;; add comments
+                                                  (replace-regexp-in-string "^[^> \n]*>+:? *" "" str))) ;; remove prompt
+                (l (length output)))
+            (insert (concat "\n\n"
+                            (if (> (length output) 4)
+                                (substring output 0 (- l 4))
+                                output)))))))) ;; remove newline and spaces
+
 
  (defun run-clojure-no-prompt ()
    (interactive)
@@ -154,34 +174,57 @@
                    (read-from-minibuffer "Command:" "clojure" nil nil 'clj-repl-command-history))))
    (run-clojure-command cmd))
 
- (defun run-clojure-command (cmd)
-   (let ((dd (if (and (fboundp 'clojure-project-root-path)
-                    (stringp (clojure-project-root-path)))
-               (clojure-project-root-path)
-             (let ((dir-locals-dir (car (dir-locals-find-file (buffer-file-name)))))
-               (if dir-locals-dir
-                   dir-locals-dir
-                 default-directory))))
-       (cb (current-buffer)))
-   (cd dd)
-   (add-to-list 'clj-repl-command-history cmd)
-   (run-lisp cmd)
-   (switch-to-buffer cb)
-   (switch-to-buffer-other-window "*inferior-lisp*")))
+ (defun run-clojure-network (host port)
+   (interactive (list
+                 (if (boundp 'clj-repl-host)
+                     (let ((first-command (car clj-repl-host))
+                           (rest-commands (if clj-repl-host-history
+                                              (append (cdr clj-repl-host) clj-repl-host-history)
+                                            (cdr clj-repl-host))))
+                       (read-from-minibuffer "Host:" first-command nil nil 'rest-commands))
+                   (read-from-minibuffer "Host:" "localhost" nil nil 'clj-repl-host-history))
+                 (if (boundp 'clj-repl-port)
+                     (let ((first-command (car clj-repl-port))
+                           (rest-commands (if clj-repl-port-history
+                                              (append (cdr clj-repl-port) clj-repl-port-history)
+                                            (cdr clj-repl-port))))
+                       (read-from-minibuffer "Port:" first-command nil nil 'rest-commands))
+                   (read-from-minibuffer "Port:" "5555" nil nil 'clj-repl-port-history))))
+   (run-clojure-command (concat "nc " host " " port) t))
 
- (let ((dd (if (and (fboundp 'clojure-project-root-path)
-                    (stringp (clojure-project-root-path)))
-               (clojure-project-root-path)
-             (let ((dir-locals-dir (car (dir-locals-find-file (buffer-file-name)))))
-               (if dir-locals-dir
-                   dir-locals-dir
-                 default-directory))))
-       (cb (current-buffer)))
-   (cd dd)
-   (add-to-list 'clj-repl-command-history cmd)
-   (run-lisp cmd)
-   (switch-to-buffer cb)
-   (switch-to-buffer-other-window "*inferior-lisp*"))
+ (defun run-clojure-command (cmd &optional net)
+   (let ((dd (if (and (fboundp 'clojure-project-root-path)
+                      (stringp (clojure-project-root-path)))
+                 (clojure-project-root-path)
+               (let ((dir-locals-dir (car (dir-locals-find-file (buffer-file-name)))))
+                 (if dir-locals-dir
+                     dir-locals-dir
+                   default-directory))))
+         (cb (current-buffer)))
+     (cd dd)
+     (if (not net)
+      (add-to-list 'clj-repl-command-history cmd))
+     (if (boundp 'lisp-environment)
+         (let ((process-environment (append process-environment clj-environment)))
+           (run-lisp cmd))
+       (run-lisp cmd))
+     (switch-to-buffer cb)
+     (switch-to-buffer-other-window "*inferior-lisp*")))
+
+
+;; (let ((dd (if (and (fboundp 'clojure-project-root-path)
+;;                    (stringp (clojure-project-root-path)))
+;;               (clojure-project-root-path)
+;;             (let ((dir-locals-dir (car (dir-locals-find-file (buffer-file-name)))))
+;;               (if dir-locals-dir
+;;                   dir-locals-dir
+;;                 default-directory))))
+;;       (cb (current-buffer)))
+;;   (cd dd)
+;;   (add-to-list 'clj-repl-command-history cmd)
+;;   (run-lisp cmd)
+;;   (switch-to-buffer cb)
+;;   (switch-to-buffer-other-window "*inferior-lisp*"))
  
  (setq lisp-function-doc-command
        "(clojure.repl/doc %s)\n")
@@ -196,7 +239,7 @@
 
  ;; make inferior-lisp repl scroll on new output
  (setq comint-scroll-to-bottom-on-output t)
- )
+ 
 
 
 ;; regex, not plain string
@@ -239,18 +282,36 @@ DEFVAR forms reset the variables to the init values. Ignores (comment) forms."
             (progn
               (goto-char (cadr forms))
               (forward-sexp)
+              (setq clojure-last-eval-function-end (point)) ;; set function end for eval-and-print
               (funcall do-region (cadr forms) (point))))
         (progn
          (let ((start (point)))
            (forward-sexp)
+           (setq clojure-last-eval-function-end (point)) ;; set function end for eval-and-print
            (funcall do-region start (point))))))))
 
-(defun clj-eval-defun (&optional and-go)
+(defun clj-eval-defun (&optional and-go buf)
   "Send the current defun to the inferior Lisp process.
 DEFVAR forms reset the variables to the init values.
 Prefix argument means switch to the Lisp buffer afterwards."
   (interactive "P")
-  (clj-do-defun 'lisp-eval-region)
+  (setq clojure-current-buffer buf) ;; comint-output-filter prints to sending buffer if this is a buffer
+  (clj-do-defun 'clj-eval-region)
+  (if and-go (switch-to-lisp t)))
+
+(defun clj-eval-defun-and-print (&optional and-go)
+  "Send the current defun to the inferior Lisp process.
+DEFVAR forms reset the variables to the init values.
+Prefix argument means switch to the Lisp buffer afterwards."
+  (interactive "P")
+  (clj-eval-defun and-go (current-buffer)))
+
+(defun clj-eval-region (start end &optional and-go)
+  "Send the current region to the inferior Lisp process.
+Prefix argument means switch to the Lisp buffer afterwards."
+  (interactive "r\nP")
+  (comint-send-region (inferior-lisp-proc) start end)
+  (comint-send-string (inferior-lisp-proc) "\n")
   (if and-go (switch-to-lisp t)))
 
 (defun inf-clj-eval-defun (&optional and-go)
