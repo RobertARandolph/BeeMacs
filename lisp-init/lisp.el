cbda750f8d2420d0c5b85379305833a9e76a4c8a
Robert Randolph <audiolabs@gmail.com>
2023-04-30 23:09:58 -0400

    fix opeaators

--- lisp-init/lisp.el
+++ lisp-init/lisp.el
@@ -16,34 +16,55 @@
 (use-package clojure-mode
   :hook (lisp-mode-hooks . clojure-mode-hook))
 
+(defun rebl-eval-last-sexp ()
+  (interactive)
+  (let* ((bounds (cider-last-sexp 'bounds))
+         (s (cider-last-sexp))
+         (reblized (concat "(cognitect.rebl/inspect " s ")")))
+    (cider-interactive-eval reblized nil bounds (cider--nrepl-print-request-map))))
+
+;; Similar to C-M-x, but sends to REBL
+(defun rebl-eval-defun-at-point ()
+  (interactive)
+  (let* ((bounds (cider-defun-at-point 'bounds))
+         (s (cider-defun-at-point))
+         (reblized (concat "(cognitect.rebl/inspect " s ")")))
+    (cider-interactive-eval reblized nil bounds (cider--nrepl-print-request-map))))
+
+
+(local-set-key (kbd "C-S-x") #'rebl-eval-defun-at-point)
+(local-set-key (kbd "C-x C-r") #'rebl-eval-last-sexp)
+
 (use-package cider
   :ensure t
-  :config (progn
-            (setq cider-print-fn 'fipp)
-            (setq clojure-toplevel-inside-comment-form t)
-            (define-key clojure-mode-map (kbd "C-z") 'cider-jack-in-clj)
-            (define-key clojure-mode-map (kbd "C-c C-z") 'cider-jack-in)
-            (define-key clojure-mode-map (kbd "C-M-x") 'cider-eval-defun-to-comment) ;; primary eval command
-            (define-key cider-mode-map (kbd "C-M-S-x") 'cider-pprint-eval-defun-to-comment)
-            (define-key cider-mode-map (kbd "C-c C-e") 'cider-eval-defun-to-comment)
-            (define-key cider-mode-map (kbd "C-x C-e") 'cider-eval-last-sexp)
-            (define-key cider-mode-map (kbd "C-x C-S-e") 'cider-pprint-eval-last-sexp)
-            (define-key cider-mode-map (kbd "C-c C-v") 'cider-doc)
-            (define-key cider-mode-map (kbd "C-c C-S-v") 'cider-clojuredocs)
-            (define-key cider-mode-map (kbd "C-c C-f") 'cider-doc)
-            (define-key cider-mode-map (kbd "C-c C-S-f") 'cider-clojuredocs)
-            (define-key cider-mode-map (kbd "C-c C-s") 'clj-show-function-source)
-            (define-key cider-mode-map (kbd "C-c C-S-s") 'cider-clojuredocs)
-            (define-key cider-mode-map (kbd "C-c C-v") 'cider-doc)
-            (define-key cider-mode-map (kbd "C-c C-S-v") 'cider-clojuredocs)
-            (define-key cider-mode-map (kbd "C-c C-a") 'cider-doc)
-            (define-key cider-mode-map (kbd "C-c C-S-a") 'cider-doc)
-            (define-key cider-mode-map (kbd "C-c C-l") 'cider-load-buffer)
-            (define-key cider-mode-map (kbd "C-c C-r") 'cider-eval-region)
-            (define-key cider-mode-map (kbd "C-c f") 'lsp-find-references)
-            (define-key cider-mode-map (kbd "C-c n") 'cider-repl-set-ns)
-            ;;(define-key clojure-mode-map (kbd "C-c C-z") 'inf-clojure)
-            ))
+  :bind (("C-S-x" . #'rebl-eval-defun-at-point)
+         ("C-x C-r" . #'rebl-eval-last-sexp))
+:config (progn
+          (setq cider-print-fn 'fipp)
+          (setq clojure-toplevel-inside-comment-form t)
+          (define-key clojure-mode-map (kbd "C-z") 'cider-jack-in-clj)
+          (define-key clojure-mode-map (kbd "C-c C-z") 'cider-jack-in)
+          (define-key clojure-mode-map (kbd "C-M-x") 'cider-eval-defun-to-comment) ;; primary eval command
+          (define-key cider-mode-map (kbd "C-M-S-x") 'cider-pprint-eval-defun-to-comment)
+          (define-key cider-mode-map (kbd "C-c C-e") 'cider-eval-defun-to-comment)
+          (define-key cider-mode-map (kbd "C-x C-e") 'cider-eval-last-sexp)
+          (define-key cider-mode-map (kbd "C-x C-S-e") 'cider-pprint-eval-last-sexp)
+          (define-key cider-mode-map (kbd "C-c C-v") 'cider-doc)
+          (define-key cider-mode-map (kbd "C-c C-S-v") 'cider-clojuredocs)
+          (define-key cider-mode-map (kbd "C-c C-f") 'cider-doc)
+          (define-key cider-mode-map (kbd "C-c C-S-f") 'cider-clojuredocs)
+          (define-key cider-mode-map (kbd "C-c C-s") 'clj-show-function-source)
+          (define-key cider-mode-map (kbd "C-c C-S-s") 'cider-clojuredocs)
+          (define-key cider-mode-map (kbd "C-c C-v") 'cider-doc)
+          (define-key cider-mode-map (kbd "C-c C-S-v") 'cider-clojuredocs)
+          (define-key cider-mode-map (kbd "C-c C-a") 'cider-doc)
+          (define-key cider-mode-map (kbd "C-c C-S-a") 'cider-doc)
+          (define-key cider-mode-map (kbd "C-c C-l") 'cider-load-buffer)
+          (define-key cider-mode-map (kbd "C-c C-r") 'cider-eval-region)
+          (define-key cider-mode-map (kbd "C-c f") 'lsp-find-references)
+          (define-key cider-mode-map (kbd "C-c n") 'cider-repl-set-ns)
+          )) ;;(define-key clojure-mode-map (kbd "C-c C-z") 'inf-clojure)
+
 (setq cider-jump-to-pop-to-buffer-actions
       '((display-buffer-same-window)))
 
@@ -51,7 +72,7 @@
 
 (use-package lispyville
   :config (lispyville-set-key-theme
-	       '(opeaators
+	       '(operators
 	         c-w
 	         slurp/barf-cp
 	         commentary
